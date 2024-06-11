c25067cd437ed2b1269425322869a0cd8fd5728d
Alex K <krab7153@gmail.com>
2024-06-11 09:31:14 +0200

    collection display

--- client/src/sites/UserPanel/Categories/Categories.tsx
+++ client/src/sites/UserPanel/Categories/Categories.tsx
@@ -1,6 +1,15 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+interface VideoCollection {
+  videoLibraryId: number;
+  guid: string;
+  name: string;
+  videoCount: number;
+  totalSize: number;
+}
+
+
 interface CategoriesProps {
     apiKey: string,
     libraryID: string
@@ -12,7 +21,7 @@ export const Categories = (props: CategoriesProps) => {
     const { apiKey, libraryID } = props;
 
     const [totalItems, setTotalItems] = useState(0);
-    const [collections, setCollections] = useState([]);
+    const [collections, setCollections] = useState<VideoCollection[]>([]);
 
     const [error, setError] = useState("");
 
@@ -21,7 +30,7 @@ export const Categories = (props: CategoriesProps) => {
     useEffect(() => {
         const fetchCollections = async () => {
           const url =
-            `https://video.bunnycdn.com/${libraryID}/252136/collections?page=1&itemsPerPage=100&orderBy=date&includeThumbnails=false`;
+            `https://video.bunnycdn.com/library/${libraryID}/collections?page=1&itemsPerPage=100&orderBy=date&includeThumbnails=false`;
           const options = {
             method: "GET",
             headers: {
@@ -40,9 +49,9 @@ export const Categories = (props: CategoriesProps) => {
               const data = await response.json();
               setTotalItems(data.totalItems);
     
-              setCollections(data);
-              console.log(data);
-    
+              console.log(data.items);
+              setCollections(data.items);
+
               setError("");
             }
           } catch (err) {
@@ -55,7 +64,7 @@ export const Categories = (props: CategoriesProps) => {
         };
     
         fetchCollections();
-    }, [apiKey]);
+    }, [libraryID]);
 
     const addCatalouge = () => {
         setMenuDisplayed(true);
@@ -74,9 +83,20 @@ export const Categories = (props: CategoriesProps) => {
                 </div>
 
                 <div className="mainUserPanel__categories-section__categories">
+                    {
+                      collections.map((collection) => (
+                        <div key={collection.guid} className="mainUserPanel__categories-section__categories__box mainUserPanel__categories-section__categories__box--collection">
+                            <span className="name">{collection.name}</span>
+                            <div className="data-box">
+                              <span>liczba video: {collection.videoCount}</span>
+                              <span>rozmiar: {collection.totalSize} MB</span>
+                            </div>
+                        </div>
+                      ))
+                    }
                     <div
                         onClick={addCatalouge} 
-                        className="mainUserPanel__categories-section__categories__box">
+                        className="mainUserPanel__categories-section__categories__box mainUserPanel__categories-section__categories__box--add-button">
                         <span>+ nowy katalog</span>
                     </div>
                 </div>
